0837577c386037054dbe8cdc53d906357b6f2c8e
oaihr <rhksgh0710@gmail.com>
2025-05-21 16:58:58 +0900

    리액트 useState에 배열활용, 배열 기반 출력

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Basic01 from './study/Basic01';
 import State01 from './study/State01';
-import Practice01 from './practice/Practice01';
+// import Practice01 from './practice/practice01';
+import State02 from './study/State02';
+import Export01 from './study/Export01';
 
 function App() {
   //return<Basic01/>;  // JSX기본   Basic01 컴포넌트를 가져와서 리턴
   //return<State01/>;   // useState 사용법
-  return<Practice01/>;
+  // return<Practice01/>;
+  // return<State02/>;
+  // return<Export01/>;
+  return <State02/>;
 
   /* return (
     <div className="App">
