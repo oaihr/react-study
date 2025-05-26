1dba539c467994a25353d2cfca8cab5ab06b17ef
oaihr <rhksgh0710@gmail.com>
2025-05-26 15:49:11 +0900

    리액트 라우터 경로처리, 경로별 페이지 설정

--- src/foodmarket/pages/FoodMarket.js
+++ src/foodmarket/pages/FoodMarket.js
@@ -7,14 +7,17 @@ import banner_bg from '../images/banner_bg.jpg';
 import food1 from '../images/food1.jpg';
 import food2 from '../images/food2.jpg';
 import food3 from '../images/food3.jpg';
+import Home from './Home';
 
 import foodsData from '../data/foodsData';
 import { useState } from 'react';
-import FoodCard from '../components/FoodCard';
+import { Routes, Route, Link, useNavigate } from 'react-router';
 
 function FoodMarket() {
 
     let [foods, setFoods] = useState(foodsData);
+
+    let navigate = useNavigate();
 
     return (
         <div>
@@ -22,13 +25,17 @@ function FoodMarket() {
                 <Container>
                     <Navbar.Brand href="#home">Market</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">FoodDetail</Nav.Link>
-                        <Nav.Link href="#pricing">Info</Nav.Link>
+                        {/* <Nav.Link> <Link to="/">Home</Link> </Nav.Link>
+                        <Nav.Link> <Link to="/detail">FoodDetail</Link> </Nav.Link>
+                        <Nav.Link> <Link to="/info">Info</Link> </Nav.Link> */}
+                        <Nav.Link onClick={()=>{navigate("/")}}> Home </Nav.Link>
+                        <Nav.Link onClick={()=>{navigate("/detail")}}> FoodDetail </Nav.Link>
+                        <Nav.Link onClick={()=>{navigate("/info")}}> Info </Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
 
+          
             {/*
                 이미지 사용
                 1) css 처리: 이미지 경로
@@ -52,22 +59,47 @@ function FoodMarket() {
             {/* 2. 이미지 import 함 */}
             {/* <div className='main-bg' style={{backgroundImage: 'url('+banner_bg+')'}}></div> */}
 
-            <div className='main-bg'></div> {/* 1. css로 처리함 */}
+            
+            
+{/* react-router: 접속한 경로별로 보여줄 화면을 설정하는데 도움을 주는 라이브러리 
+                                + 기타 URL 주소에 관한 각종 처리
 
-            <Container>
-                <Row>
-                    {
-                        foods.map((food, index) => {                            
-                            return(
-                                <Col md={4} sm={6}>
-                                    <FoodCard foods={foods} index={index} food={food}/>
-                                </Col>
-                            ) 
-                        })
-                    }
-                </Row>
-            </Container>
 
+                페이지 이동 처리
+
+                1) Link (= a태그)
+                    내부 요소를 눌렀을 때 페이지 이동
+                    <Link to ="이동할경로"></Link>
+
+                2) useNavigate  (= location.href 비슷)
+                    함수 형태로 호출해서 이동처리
+
+                    사용법====================================
+                    경로처리 객체를 생성
+                    let navigate = useNavigate();
+
+                    이동할 경로를 매개변수로 전달하면서 함수 사용
+                    navigate("이동할 경로");
+            */}
+            
+            <Routes>
+                <Route path='/' element={<Home foods={foods}/>}></Route>
+                <Route path='/home' element={<Home foods={foods}/>}></Route>
+                <Route path='/detail' element={<h1>/detail</h1>}></Route>
+
+                {/* <Route path='/info/company' element={<h1>/info company</h1>}></Route>
+                <Route path='/info/contact' element={<h1>/info contact</h1>}></Route> */}
+
+                {/* 서브경로 */}
+                <Route path='/info'>
+                    <Route path='company' element={<h1>/info company</h1>}></Route>
+                    <Route path='contact' element={<h1>/info contact</h1>}></Route>
+                </Route>
+                
+
+                {/* * 표시: all */}
+                <Route path='*' element={<div><h1>잘못된 경로입니다. 주소를 확인하세요</h1></div>}></Route>
+            </Routes>
         </div>
     );
 }
---
+++ src/foodmarket/pages/Home.js
@@ -0,0 +1,25 @@
+import { Nav, Navbar, Container, Card, Button, Row, Col } from 'react-bootstrap';
+import FoodCard from '../components/FoodCard';
+
+function Home( {foods} ){
+    return(
+        <div>
+            <div className='main-bg'></div> {/* 1. css로 처리함 */}
+
+            <Container>
+                <Row>
+                    {
+                        foods.map((food, index) => {                            
+                            return(
+                                <Col md={4} sm={6}>
+                                    <FoodCard foods={foods} index={index} food={food}/>
+                                </Col>
+                            ) 
+                        })
+                    }
+                </Row>
+            </Container>
+        </div>
+    );
+}
+export default Home;
