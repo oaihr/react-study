40175c301fe2bec3034686d229967d0ccb4afdde
oaihr <rhksgh0710@gmail.com>
2025-05-27 14:22:29 +0900

    스타일적용 방법(객체, css, className, state변수)

--- src/foodmarket/pages/Detail.js
+++ src/foodmarket/pages/Detail.js
@@ -12,8 +12,10 @@ function Detail({ foods }) {
 
     let [orderCount, setOrderCount] = useState(0);
 
-    let [showModal, setShowModal] = useState(true);
+    let [showModal, setShowModal] = useState(false);
+                                        // false 모달창 안뜸
 
+    let [viewClass, setViewClass] = useState('');                                    
 
     // foods 전체 정보 보유
 
@@ -62,6 +64,19 @@ function Detail({ foods }) {
         
     }, [])
 
+    // container opacity 변환 효과
+
+    useEffect(()=>{
+        
+        setTimeout(()=>{
+            setViewClass('end'); // '' -> 'end'
+                            // 'start ' -> 'start end'
+        }, 100)        
+    
+    },[])
+
+
+
 
 
 
@@ -78,16 +93,100 @@ function Detail({ foods }) {
         );
     }
 
-    return (
-        <Container>
+    
+    //style 객체 단위로 관리
+    const tempStyle = {
+        color: "orange",
+        fontSize: '20px'
+    };
+
+    const blueTextStyle = {
+        color: "blue"
+    }
+
+    const styles = {
+        redStyle:{
+            color: "red"
+        },
+        blueStyle:{
+            color: "blue"
+        },
+        fontBigBold:{
+            fontSize: "2rem",
+            fontWeight: "bold"
+        }
+        
+    }
+
+
+    /*
+    
+        조건에 따라서 스타일 적용
+    
+        가격표시 {food.price}
+            1만원 이상 -> 빨간색
+            1만원 미안 -> 파란색
+
+        food.price >= 10000    
+    */
+
+    // 1) js 객체 이용
+    const priceTextStyle = {
+        color: (food.price>=10000) ? 'red' : 'blue'
+    }
+    //<p style={priceTextStyle}>{food.price}</p>
+    //<p style={ {color: (food.price>=10000) ? 'red' : 'blue'} }>{food.price}</p>
+
+
+    // 2) js 함수 형태
+    const priceTextStyleFunc = (price)=>{
+
+        if(price > 10000)
+            return {color: "red"}
+        else
+            return {color: "blue"}
+
+        //return {color: price>=10000 ? 'red' : 'blue'}
+    }
+    // <p style={priceTextStyleFunc(food.price)}>{food.price}</p>
+
+
+    /*3) css 클래스명 연계 활용
+
+        //단일 클래스 적용
+        <p className={ food.price>=10000 ? 'price-red' : 'price-blue'}>{food.price}</p>
+
+        //다중 클래스 적용
+        <p className={ food.price>=10000 ? 'price-red text-strong' : 'price-blue text-strong'}>{food.price}</p>
+
+            +연산
+            <p className={ 'text-strong '+(food.price>=10000 ? 'price-red' : 'price-blue') }>{food.price}</p>
+            
+            join 함수
+            <p className={ ['text-strong', food.price>=10000 ? 'price-red' : 'price-blue'].join(" ")  }>{food.price}</p>
+
+            변수
+            const priceClassName = 'text-strong price-red'
+
+            백틱 활용 `
+            <p className={ `text-strong, ${food.price>=10000 ? 'price-red' : 'price-blue'} ` }>{food.price}</p> 
+
+    */
+
+
+    return (                // 'start    end'
+        <Container className={'start ' +viewClass}>
             <Row>
                 <Col md={6}>
                     <img src={process.env.PUBLIC_URL + food.imageUrl} width="100%" />
                 </Col>
                 <Col md={6}>
                     <h4 style={{ paddingTop: '30px' }}>{food.title}</h4>
-                    <p>{food.content}</p>
-                    <p>{food.price}</p>
+                    {/* <p style={tempStyle}>{food.content}</p> */}
+                    <p style={styles.fontBigBold}>{food.content}</p>
+                    
+                    <p className={ ['text-strong', food.price>=10000 ? 'price-red' : 'price-blue'].join(" ")  }>{food.price}</p>
+                    
                     <p>
                         <Button variant="dark" onClick={() => {
                             if (orderCount > 0)
